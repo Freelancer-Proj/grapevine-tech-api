9d0f47cb0172dd23a79fb28a25f99aedc3a3e3b7
duy-nguyen1610 <nguyenphuocduy1610@gmail.com>
2020-07-29 18:13:37 +0700

    Fix bug

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -337,9 +337,4 @@ ActiveAdmin.setup do |config|
     config.default_namespace = false
     # ...
   end
-
-  class ActiveAdmin::Devise::SessionsController
-    skip_before_action :verify_authenticity_token
-    protect_from_forgery with: :null_session
-  end
 end
