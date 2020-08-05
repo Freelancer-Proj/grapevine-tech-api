54357d302b514b59c00d2ac0b26563461b307612
duy-nguyen1610 <nguyenphuocduy1610@gmail.com>
2020-08-05 09:29:54 +0700

    Add time create and update

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,4 +1,4 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :title, :path, :tags, :desc, :content, :images
+  attributes :id, :title, :path, :tags, :desc, :content, :images, :created_at, :updated_at
   has_many :staffs
 end
